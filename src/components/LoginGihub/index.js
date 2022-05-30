c7dd61ec6dd9f54b9bbca3f05c36f59920174806
Bianca <bianca.fraccaroli@bandtec.com.br>
2022-05-30 11:09:07 -0300

    github fix

--- src/components/LoginGihub/index.js
+++ src/components/LoginGihub/index.js
@@ -17,7 +17,8 @@ export default function LoginGithub(props) {
     
           console.log(newUrl[1])
           axios.post("https://node-server-balancefy.herokuapp.com/authenticate", {
-            code: newUrl[1]
+            code: newUrl[1],
+            page: props.page === "register" ? "Cadastro" : "Login"
           }).then((res) => {
             console.log(res)
             onSuccess(res)
